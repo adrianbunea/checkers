e0c4b142d76f8abfeb5b7846a41b5e4ebee15232
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-09-01 19:18:53 +0300

    Create Board class

---
+++ Checkers.Tests/BoardTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Checkers.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void Board_NoParameters_StartsAsEmpty()
+        {
+            Board board = new Board();
+            int[,] expectedSquares = new int[10, 10];
+            int[,] actualSquares = board.squares;
+            CollectionAssert.AreEqual(expectedSquares, actualSquares);
+        }
+
+        [TestMethod]
+        public void Initialize_NoParameters_SetsPiecesCorrectly()
+        {
+            Board board = new Board();
+            int[,] expectedSquares = new int[10, 10] 
+            {
+                { 0, -1, 0, -1, 0, -1, 0, -1, 0, -1 },
+                { -1, 0, -1, 0, -1, 0, -1, 0, -1, 0 },
+                { 0, -1, 0, -1, 0, -1, 0, -1, 0, -1 },
+                { -1, 0, -1, 0, -1, 0, -1, 0, -1, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+                { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 },
+                { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+                { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 }
+            };
+
+            board.Initialize();
+            int[,] actualSquares = board.squares;
+            CollectionAssert.AreEqual(expectedSquares, actualSquares);
+        }
+    }
+}
---
+++ Checkers/Board.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public class Board
+    {
+        public int[,] squares;
+
+        public Board()
+        {
+            squares = new int[10, 10];
+        }
+
+        public void Initialize()
+        {
+            SetBlackPieces();
+            SetWhitePieces();
+        }
+
+        private void SetBlackPieces()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    squares[i, j] = -(i + j) % 2;
+                }
+        }
+        private void SetWhitePieces()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    squares[i+6, j] = (i + j) % 2;
+                }
+        }
+    }
+}
